b8cc362344b48bb4bd6876d02e851c43e05264c4
tshinig <devworldc2@gmail.com>
2024-08-21 10:43:40 -0300

    fix: Delete Test Cy

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -4,7 +4,7 @@ export const meta: MetaFunction = () => [{ title: "Remix Notes" }];
 
 export default function Index() {
   return (
-    <main className="relative min-h-screen bg-white sm:flex sm:items-center sm:justify-center">
+    <main>
       Hello World.
     </main>
   );
