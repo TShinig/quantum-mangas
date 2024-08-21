a8c0b8b789b16955222b5137315740e0454912a5
tshinig <devworldc2@gmail.com>
2024-08-21 10:29:41 -0300

    fix: Remove unsage code.

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,12 +1,8 @@
 import type { MetaFunction } from "@remix-run/node";
-import { Link } from "@remix-run/react";
-
-import { useOptionalUser } from "~/utils";
 
 export const meta: MetaFunction = () => [{ title: "Remix Notes" }];
 
 export default function Index() {
-  const user = useOptionalUser();
   return (
     <main className="relative min-h-screen bg-white sm:flex sm:items-center sm:justify-center">
       Hello World.
