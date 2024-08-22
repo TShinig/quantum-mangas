c57acd323499d7547e8d829326a27f47c83fcb68
tshinig <devworldc2@gmail.com>
2024-08-21 23:35:44 -0300

    feat: :sparkles: Adicionar integração com API do MangaDex e exibir dados no navegador

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,36 +1,52 @@
 import { json, type MetaFunction } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 
-export const meta: MetaFunction = () => [{ title: "Remix Notes" }];
+export const meta: MetaFunction = () => [{ title: "Quantum Mangás" }];
 
 export const loader = async () => {
-  const BASE_URL = 'https://api.mangadex.org'
-  const BASE_IMAGE_URL = 'https://uploads.mangadex.org'
-  
-  const result = await fetch(`${BASE_URL}/manga`);
-  console.log(result.data);
+  const BASE_URL = 'https://api.mangadex.org';
+	
+  // BASE_IMAGE_URL 'https://uploads.mangadex.org';
+	
+  const BASE_MANGA = await fetch(`${BASE_URL}/manga`);
+	const BASE_AUTHOR = await fetch(`${BASE_URL}/author`);
+	const BASE_GROUP = await fetch(`${BASE_URL}/group`);
+	const BASE_COVER = await fetch(`${BASE_URL}/cover`);
+	const BASE_CHAPTER = await fetch(`${BASE_URL}/chapter`);
+	const BASE_STATISTICS = await fetch(`${BASE_URL}/statistics`);
 
-  const items = [
-    { id: 1, title: 'title' },
-    { id: 2, title: 'description' },
-  ];
-
-  return json({ items, result });
-}
+  return json(
+    {
+      BASE_MANGA,
+      BASE_AUTHOR,
+      BASE_GROUP,
+      BASE_COVER,
+      BASE_CHAPTER,
+      BASE_STATISTICS 
+    }
+  );
+};
 
 export default function Index() {
-  const { items, result } = useLoaderData();
+  const {
+    BASE_MANGA,
+    BASE_AUTHOR,
+    BASE_GROUP,
+    BASE_COVER,
+    BASE_CHAPTER,
+    BASE_STATISTICS 
+  } = useLoaderData<typeof loader>();
 
   return (
     <main>
       <div className="whitespace-pre">
-        { JSON.stringify(result, null, 2) }
+        { JSON.stringify(BASE_MANGA, null, 2) }
+        { JSON.stringify(BASE_AUTHOR, null, 2) }
+        { JSON.stringify(BASE_GROUP, null, 2) }
+        { JSON.stringify(BASE_COVER, null, 2) }
+        { JSON.stringify(BASE_CHAPTER, null, 2) }
+        { JSON.stringify(BASE_STATISTICS, null, 2) }
       </div>
-      {items.map(({ id, title }) => (
-        <div key={id}>
-          <h2>{title}</h2>
-        </div>
-      ))}
     </main>
   );
 }
